492f697e458f96de897afa887827f318b60055f4
Fredy <fredrodrigues795@gmail.com>
2023-04-10 22:33:56 +0100

    :heavy_minus_sign:ejs knex mysql2 desinstalados

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ import session from "express-session";
 import router from "./routes/routes.js";
 import "./database/connection.js";
 
-// View engine
-app.set("view engine", "ejs")
-
 app.use(session({secret: process.env.SECRET, cookie: {maxAge: 604800000}}))
-// Static
-app.use(express.static("public"))
 
 // BodyParser
 app.use(bodyParser.urlencoded({extended: false}))
