2dd2f8ef0535b0a1eaf22b431d0634b813fe7134
Fredy <fredrodrigues795@gmail.com>
2023-05-14 16:38:44 +0100

    :art:Cluster adicionado as variáveis de ambiente

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 mongoose.Promise = global.Promise
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SENHA}@cluster1.im5qg0x.mongodb.net/minhamente?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SENHA}@${process.env.DB_CLUSTER}.im5qg0x.mongodb.net/minhamente?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 .then( () => console.log("Conectado ao banco de dados"))
