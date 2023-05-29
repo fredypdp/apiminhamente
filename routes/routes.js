5ed9ed3537fd68f1df4ce9bbe32318f0d8859419
Fredy <fredrodrigues795@gmail.com>
2023-05-29 17:06:29 +0100

    :bug:Correção no caminho da pasta temp no multer 5

--- routes/routes.js
+++ routes/routes.js
@@ -16,8 +16,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const folder = path.resolve(__dirname, 'temp/')
-        console.log(folder);
+        const folder = path.resolve(__dirname, '../temp')
+		
         // checa se a pasta existe, se existe, chama o callback
         if (fs.existsSync(folder)) {
             cb(null, folder)
